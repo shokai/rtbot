0072b98fdec57926779fa4dd5ebfb561f7282c5a
Sho Hashimoto <hashimoto@shokai.org>
2012-05-19 16:10:11 +0900

    add caution message in db_migrate.rb

--- bin/db_migrate.rb
+++ bin/db_migrate.rb
@@ -3,4 +3,12 @@ require File.dirname(__FILE__)+'/../bootstrap'
 Bootstrap.init :db
 
 require 'dm-migrations'
+
+puts 'migration will DELETE and RESET your DB.'
+puts 'initialize DB?'
+puts '[y/N]'
+exit unless STDIN.gets =~ /y/i
+
 DataMapper.auto_migrate!
+
+puts 'DB migrate success!!'
