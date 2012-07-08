2133a69754c054ca24b7eb4b3980b327f6aee762
Sho Hashimoto <hashimoto@shokai.org>
2012-07-09 06:18:49 +0900

    move stats.rb to project root/helpers/stats.rb

---
+++ helpers/stats.rb
@@ -0,0 +1,20 @@
+
+def stats
+  {
+    :tweet => {
+      :count => Status.count,
+      :last_checked_at => Status.last_checked_at
+    },
+    :user => {
+      :count => User.count,
+      :last_checked_at => User.last_checked_at
+    }
+  }
+end
+
+if __FILE__ == $0
+  require File.expand_path '../bootstrap', File.dirname(__FILE__)
+  Bootstrap.init :db
+
+  p stats
+end
